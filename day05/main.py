7a85bbcf8bd14b3f92a79a62780e19d9db34ffde
Jacob Lambert <jacob.lambert@gmail.com>
2023-12-05 02:00:42 -0800

    Refactor day05

--- day05/main.py
+++ day05/main.py
@@ -16,17 +16,17 @@ class Transformer:
         self.tuples.sort()
 
     def transform(self, seed: int, seed_count: int) -> [(int, int)]:
-        for l, r, range_count in self.tuples:
+        for l, r, range_count in self.tuples: # First check if seed is in any range
             if l <= seed < l + range_count:
                 if seed_count <= range_count - (seed - l):
                     return [(r + (seed - l), seed_count)]
                 consumed = range_count - (seed - l)
                 return [(r + (seed - l), consumed)] + self.transform(seed + consumed, seed_count - consumed)
-        for l, _, _ in self.tuples:
+        for l, _, _ in self.tuples: # If not, check whether an overlapping range is to its right
             if seed < l < seed + seed_count:
                 consumed = (l - seed)
                 return [(seed, consumed)] + self.transform(seed + consumed, seed_count - consumed)
-        return [(seed, seed_count)]
+        return [(seed, seed_count)] # The seed range overlaps no ranges
 
     def transform_many(self, seed_ranges: [(int, int)]) -> [(int, int)]:
         out = []
@@ -42,20 +42,18 @@ def seeds_to_locs(seed: int, seed_count: int) -> [(int, int)]:
             cur = m.transform_many(cur)
         return cur
 
-p1 = float("inf")
-p2 = float("inf")
+def min_loc(seed_pairs: [(int, int)]) -> int:
+    m = float("inf")
+    for seed, count in seed_pairs:
+        locs = seeds_to_locs(seed, count)
+        min_loc = min(locs, key=lambda x: x[0])[0]
+        m = min(min_loc, m)
+    return m
 
-for seed in seeds:
-    locs = seeds_to_locs(seed, 1)
-    min_loc = min(locs, key=lambda x: x[0])[0]
-    p1 = min(min_loc, p1)
-
-for i in range(len(seeds) // 2):
-    start = seeds[2 * i]
-    count = seeds[2*i + 1]
-    locs = seeds_to_locs(start, count)
-    min_loc = min(locs, key=lambda x: x[0])[0]
-    p2 = min(min_loc, p2)
-
+p1 = min_loc([(seed, 1) for seed in seeds])
 print(f"Part one: {p1}")
+
+p2 = min_loc([
+    (seeds[2 * i], seeds[2*i + 1]) for i in range(len(seeds) // 2)
+])
 print(f"Part two: {p2}")
