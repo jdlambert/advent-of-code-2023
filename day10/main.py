3d8121e2bb31401ac5a4159e934ad0eb9a3b10b0
Jacob Lambert <jacob.lambert@gmail.com>
2023-12-10 12:35:56 -0800

    Further simplify day10

--- day10/main.py
+++ day10/main.py
@@ -9,79 +9,38 @@ for si, line in enumerate(grid):
     if sj > -1:
         break
 
-pipes = {
-    '|': ((-1, 0), (1, 0)),
-    '-': ((0, -1), (0, 1)),
-    'L': ((-1, 0), (0, 1)),
-    'J': ((-1, 0), (0, -1)),
-    '7': ((0, -1), (1, 0)),
-    'F': ((0, 1), (1, 0)),
+UP, DOWN, LEFT, RIGHT = ((-1, 0), (1, 0), (0, -1), (0, 1))
+
+pipe_flow = {
+    UP: {'|': UP, '7': LEFT, 'F': RIGHT},
+    DOWN: {'|': DOWN, 'L': RIGHT, 'J': LEFT},
+    LEFT: {'-': LEFT, 'L': UP, 'F': DOWN},
+    RIGHT: {'-': RIGHT, '7': DOWN, 'J': UP},
 }
 
-def in_bounds(i, j):
-    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
-
-def neighbors(i0, j0):
-    for i in range(i0 - 1, i0 + 2):
-        for j in range(j0 - 1, j0 + 2):
-            yield (i, j)
-
-# Determine what type of pipe S is acting as
-
-adj_pipes = []
-for (di, dj) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+s_dirs = []
+for (di, dj) in [UP, DOWN, LEFT, RIGHT]:
     i, j = si + di, sj + dj
-    if in_bounds(i, j):
-        pipe = grid[i][j]
-        if pipe not in pipes:
-            continue
-        for ddi, ddj in pipes[pipe]:
-            if (i + ddi, j + ddj) == (si, sj):
-                adj_pipes.append((di, dj))
-assert len(adj_pipes) == 2
-for pipe, deltas in pipes.items():
-    if sorted(deltas) == sorted(adj_pipes):
-         s_pipe = pipe
-         break
+    if 0 <= i < len(grid) and 0 <= j < len(grid[0]):
+        if grid[i][j] in pipe_flow[(di, dj)]:
+            s_dirs.append((di, dj))
 
-pipes['S'] = pipes[s_pipe]
-
-# Discover the loop via DFS
-
-frontier = [(si, sj)]
+i, j = si, sj
+di, dj = s_dirs[0]
 loop = set()
-while frontier:
-    i, j = frontier.pop()
-    if (i, j) in loop:
-        continue
+while (i, j) not in loop:
     loop.add((i, j))
-    for di, dj in pipes[grid[i][j]]:
-        frontier.append((i + di, j + dj))
+    i, j = i + di, j + dj
+    di, dj = pipe_flow[(di, dj)].get(grid[i][j], (0, 0))
 
 interior = set()
-
 for i, line in enumerate(grid):
-    inn = False
+    ext = True
     for j, tile in enumerate(line):
-        if tile == "S":
-            tile = s_pipe
-        if (i, j) in loop and tile in "JL|":
-            inn = not inn
-        if (i, j) not in loop and inn:
+        if (i, j) in loop and tile in "JL|" or tile == "S" and UP in s_dirs:
+            ext = not ext
+        if (i, j) not in loop and not ext:
             interior.add((i, j))
-
-mapping = {
-    "F": "\u250F",
-    "J": "\u251B",
-    "L": "\u2517",
-    "7": "\u2513",
-    "|": "\u2503",
-    "-": "\u2501",
-}
-
-mapping['S'] = mapping[s_pipe]
-
-# Optionally, print a visual aid
 
 if 'p' in sys.argv:
     for i, line in enumerate(grid):
@@ -89,7 +48,7 @@ if 'p' in sys.argv:
             if (i, j) in interior:
                 print(f"\x1b[0;30;41m \x1b[0m", end='')
             elif (i, j) in loop:
-                print(mapping[tile], end='')
+                print(tile, end='')
             else:
                 print(f"\x1b[0;30;47m \x1b[0m", end='')
         print("")
