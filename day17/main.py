d36209259cedd0e12f52cfffcac7d5d2e652ac55
Jacob Lambert <jacob.lambert@gmail.com>
2023-12-16 23:18:06 -0800

    Clean up day17

--- day17/main.py
+++ day17/main.py
@@ -6,41 +6,29 @@ with open("example.txt" if "x" in sys.argv else "input.txt") as f:
 
 grid = [[int(c) for c in line] for line in data.splitlines()]
 
-N, E, S, W = (-1, 0), (0, 1), (1, 0), (0, -1)
-
-def in_bounds(i, j):
-    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
-
-def turn(dir):
-    return {
-        N: [N, E, W],
-        W: [N, S, W],
-        S: [E, W, S],
-        E: [N, S, E],
-    }[dir]
+dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
 def pathfind(lo, hi):
-    heap = [(0, (d[0], d[1]), d, 1) for d in (E, S)]
+    heap = [(0, (d[0], d[1]), d, 1) for d in dirs[:2]]
     heapq.heapify(heap)
     seen = set()
 
     while heap:
-        heat, pos, d, consec = heapq.heappop(heap)
+        heat, (i, j), (di, dj), consec = heapq.heappop(heap)
         if consec > hi:
             continue
-        if not in_bounds(*pos):
+        if not (0 <= i < len(grid) and 0 <= j < len(grid[0])):
             continue
-        if (pos, d, consec) in seen:
+        if ((i, j), (di, dj), consec) in seen:
             continue
-        seen.add((pos, d, consec))
-        heat += grid[pos[0]][pos[1]]
-        if pos == (len(grid) - 1, len(grid[0]) - 1):
+        seen.add(((i, j), (di, dj), consec))
+        heat += grid[i][j]
+        if (i, j) == (len(grid) - 1, len(grid[0]) - 1):
             return heat
-        if consec < lo:
-            heapq.heappush(heap, (heat, (pos[0] + d[0], pos[1] + d[1]), d, consec + 1))
-        else:
-            for t in turn(d):
-                heapq.heappush(heap, (heat, (pos[0] + t[0], pos[1] + t[1]), t, consec + 1 if t == d else 1))
+        heapq.heappush(heap, (heat, (i + di, j + dj), (di, dj), consec + 1))
+        if consec >= lo:
+            for ndi, ndj in [(ndi, ndj) for (ndi, ndj) in dirs if (ndi, ndj) not in [(-di, -dj), (di, dj)]]:
+                heapq.heappush(heap, (heat, (i + ndi, j + ndj), (ndi, ndj), 1))
 
 print(f"Part one: {pathfind(0, 3)}")
 print(f"Part two: {pathfind(4, 10)}")
