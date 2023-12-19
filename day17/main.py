3713d8d1ec9a9957e23674b0c98b220474dd6c14
Jacob Lambert <jacob.lambert@gmail.com>
2023-12-19 03:24:18 -0800

    Try out a complex number day17

--- day17/main.py
+++ day17/main.py
@@ -4,31 +4,28 @@ import heapq
 with open("example.txt" if "x" in sys.argv else "input.txt") as f:
     data = f.read()
 
-grid = [[int(c) for c in line] for line in data.splitlines()]
-
-dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+lines = data.splitlines()
+grid = {i + j*1j: int(c) for i, line in enumerate(lines) for j, c in enumerate(line)}
+H, W = len(lines), len(lines[0])
+END = H - 1 + (W - 1)*1j
 
 def pathfind(lo, hi):
-    heap = [(len(grid) + len(grid[0]) - 3, (d[0], d[1]), d, 1) for d in dirs[:2]]
+    heap = [(0, id(d), d, d, 1) for d in (1, 1j)]
     heapq.heapify(heap)
     seen = set()
 
     while heap:
-        a, (i, j), (di, dj), consec = heapq.heappop(heap)
-        if consec > hi:
+        a, _, p, dp, consec = heapq.heappop(heap)
+        if p not in grid or consec > hi or (p, dp, consec) in seen:
             continue
-        if not (0 <= i < len(grid) and 0 <= j < len(grid[0])):
-            continue
-        if ((i, j), (di, dj), consec) in seen:
-            continue
-        seen.add(((i, j), (di, dj), consec))
-        a += grid[i][j]
-        if (i, j) == (len(grid) - 1, len(grid[0]) - 1):
+        seen.add((p, dp, consec))
+        a += grid[p]
+        if p == END:
             return a
-        heapq.heappush(heap, (a - di - dj, (i + di, j + dj), (di, dj), consec + 1))
+        heapq.heappush(heap, (a, id(p + dp), p + dp, dp, consec + 1))
         if consec >= lo:
-            for ndi, ndj in [(ndi, ndj) for (ndi, ndj) in dirs if (ndi, ndj) not in [(-di, -dj), (di, dj)]]:
-                heapq.heappush(heap, (a - ndi - ndj, (i + ndi, j + ndj), (ndi, ndj), 1))
+            for ndp in (dp * 1j, dp * -1j):
+                heapq.heappush(heap, (a, id(p + ndp), p + ndp, ndp, 1))
 
 print(f"Part one: {pathfind(0, 3)}")
 print(f"Part two: {pathfind(4, 10)}")
