061e052d387ec63c70768f0edad221067f91e4d5
Jacob Lambert <jacob.lambert@gmail.com>
2023-12-22 22:59:08 -0800

    Single-walk day21

--- day21/main.py
+++ day21/main.py
@@ -1,5 +1,5 @@
 import sys
-import itertools
+import collections
 
 with open("example.txt" if "x" in sys.argv else "input.txt") as f:
     data = f.read()
@@ -12,28 +12,52 @@ grid = {
 }
 
 N = max(int(p.real + 1) for p in grid)
-mid = N//2
-last = N - 1
-S = mid + mid*1j
 
-def walk(start, steps):
-    ps = {start}
-    for _ in range(steps):
-        ps = {p + dp for p in ps for dp in (1, -1, 1j, -1j) if p + dp in grid}
-    return len(ps)
+p1 = 0
+ps = collections.deque([(N//2 + N//2*1j, 0)])
+evens = set()
+odds = set()
+while ps:
+    p, s = ps.popleft()
+    if s == 65:
+        p1 = len(evens)
+    if s == (5 * N) // 2 + 1:
+        break
+    if p.real%N + p.imag%N*1j not in grid or p in evens or p in odds:
+        continue
+    if s & 1:
+        odds.add(p)
+    else:
+        evens.add(p)
+    ps.extend((p + dp, s + 1) for dp in (1, -1, 1j, -1j))
 
-p1 = walk(S, 64)
 print(f"Part one: {p1}")
 
-evens = walk(S, 2 * N)
-odds = walk(S, 2 * N + 1)
-corners = (0, last, last*1j, last + last*1j)
-A = sum(walk(p, (3 * N - 3) // 2) for p in corners)
-B = sum(walk(p, (N - 3) // 2) for p in corners)
-midpoints = (mid, mid*1j, last + mid*1j, mid + last*1j)
-T = sum(walk(p, N - 1) for p in midpoints)
+def constrain(s, i0, i1, j0, j1):
+    return len([p for p in s if i0 <= p.real < i1 and j0 <= p.imag < j1])
+
+EVEN = constrain(evens, 0, N, 0, N)
+ODD = constrain(odds, 0, N, 0, N)
+BIG_DIAG = sum(constrain(odds, *c) for c in [
+    (N, 2*N, N, 2*N),
+    (N, 2*N, -N, 0),
+    (-N, 0, N, 2*N),
+    (-N, 0, -N, 0)
+])
+LIL_DIAG = sum(constrain(odds, *c) for c in [
+    (-2*N, -N, N, 2*N),
+    (N, 2*N, 2*N, 3*N),
+    (2*N, 3*N, -N, 0),
+    (-N, 0, -2*N, -N)
+])
+TIP = sum(constrain(odds, *c) for c in [
+    (-2*N, -N, 0, N),
+    (0, N, 2*N, 3*N),
+    (2*N, 3*N, 0, N),
+    (0, N, -2*N, -N)
+])
 
 Q = 26501365 // N
-p2 = (Q - 1)**2*odds + Q**2*evens + (Q - 1)*A + Q*B + T
+p2 = (Q - 1)**2*ODD + Q**2*EVEN + (Q - 1)*BIG_DIAG + Q*LIL_DIAG + TIP
 
 print(f"Part two: {p2}")
